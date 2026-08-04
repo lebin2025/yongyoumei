f3d408cc8793ca3edffd6eb49885a45b9e972232
zheng-lebin <zhenglebin@example.com>
2026-08-04 22:51:29 +0800

    导出和预览统一加宽至1600px，横向扁版效果

--- js/app.js
+++ js/app.js
@@ -627,7 +627,7 @@ const ProjectActions = {
     const html = Table.buildDocHTML(data, rows);
     const tempDiv = document.createElement('div');
     tempDiv.innerHTML = html;
-    tempDiv.style.cssText = 'position:absolute;left:-9999px;top:0;width:1400px;';
+    tempDiv.style.cssText = 'position:absolute;left:-9999px;top:0;width:1600px;';
     document.body.appendChild(tempDiv);
 
     try {
@@ -684,7 +684,7 @@ const ProjectActions = {
     const html = Table.buildDocHTML(data, rows);
     const temp = document.createElement('div');
     temp.innerHTML = html;
-    temp.style.cssText = 'position:absolute;left:-9999px;top:0;width:1400px;';
+    temp.style.cssText = 'position:absolute;left:-9999px;top:0;width:1600px;';
     document.body.appendChild(temp);
 
     // 使用 html2canvas + jsPDF
